aad6f83d48400e05fb1acb6ea098186458af9582
SwarzChen <aaaa102234@gmail.com>
2022-05-28 21:39:35 +0800

    ⚡️ [main/grpc] add rpc client to player event handler

--- src/main/java/m1pro/parkour/god/rlenvironmentplugin/PlayerEvents.java
+++ src/main/java/m1pro/parkour/god/rlenvironmentplugin/PlayerEvents.java
@@ -1,5 +1,6 @@
 package m1pro.parkour.god.rlenvironmentplugin;
 
+import io.grpc.ManagedChannelBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 
 public class PlayerEvents implements Listener {
     ArrayList<IntegerLocation> PastLocationList = new ArrayList<>();
+    DataTransfer client = new DataTransfer(ManagedChannelBuilder.forTarget("localhost:50051").usePlaintext().build());
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
@@ -31,7 +33,11 @@ public class PlayerEvents implements Listener {
 
             player.sendMessage(ChatColor.LIGHT_PURPLE + "New block explored in location : (" + realLocationX + ", " + realLocationY + ", " + realLocationZ + ")");
             player.sendMessage(ChatColor.GREEN + "Block type: " + blockNameBelowPlayer);
+
             this.PastLocationList.add(integerLocation);
+
+            // Send data to python server
+            this.client.sendDataToModel(xInteger, yInteger, zInteger, "ALIVE");
         }
     }
 
@@ -45,6 +51,14 @@ public class PlayerEvents implements Listener {
                 player.setHealth(20.0);
                 player.teleport(new Location(player.getWorld(), 100, 63, 109));
                 player.sendMessage(ChatColor.RED + "Player " + player.getName() + "is dead");
+
+                Location locationBelowPlayer = player.getLocation().subtract(0, 1, 0);
+                int xInteger = (int) Math.floor(locationBelowPlayer.getX());
+                int yInteger = (int) Math.floor(locationBelowPlayer.getY());
+                int zInteger = (int) Math.floor(locationBelowPlayer.getZ());
+
+                // Send data to python server
+                this.client.sendDataToModel(xInteger, yInteger, zInteger, "DEAD");
             }
         }
     }
