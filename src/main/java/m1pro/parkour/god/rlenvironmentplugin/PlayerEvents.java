f6864e73b889edfea36d2f028be25e067050db33
SwarzChen <aaaa102234@gmail.com>
2022-05-28 14:06:44 +0800

    🍰 [main/auto_respawn] finish auto respawn

--- src/main/java/m1pro/parkour/god/rlenvironmentplugin/PlayerEvents.java
+++ src/main/java/m1pro/parkour/god/rlenvironmentplugin/PlayerEvents.java
@@ -5,7 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.ArrayList;
@@ -16,27 +16,36 @@ public class PlayerEvents implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Location realTimeLocation = player.getLocation();
-        int xInteger = (int) Math.floor(realTimeLocation.getX());
-        int yInteger = (int) Math.floor(realTimeLocation.getY());
-        int zInteger = (int) Math.floor(realTimeLocation.getZ());
+        Location locationBelowPlayer = player.getLocation().subtract(0, 1, 0);
+        int xInteger = (int) Math.floor(locationBelowPlayer.getX());
+        int yInteger = (int) Math.floor(locationBelowPlayer.getY());
+        int zInteger = (int) Math.floor(locationBelowPlayer.getZ());
         IntegerLocation integerLocation = new IntegerLocation(xInteger, yInteger, zInteger);
 
-        if(!this.PastLocationList.contains(integerLocation)) {
+        String blockNameBelowPlayer = locationBelowPlayer.getBlock().getType().toString();
+
+        if(!this.PastLocationList.contains(integerLocation) && !blockNameBelowPlayer.equals("AIR")) {
             String realLocationX = String.valueOf(xInteger);
             String realLocationY = String.valueOf(yInteger);
             String realLocationZ = String.valueOf(zInteger);
 
             player.sendMessage(ChatColor.LIGHT_PURPLE + "New block explored in location : (" + realLocationX + ", " + realLocationY + ", " + realLocationZ + ")");
+            player.sendMessage(ChatColor.GREEN + "Block type: " + blockNameBelowPlayer);
             this.PastLocationList.add(integerLocation);
         }
     }
 
     @EventHandler
-    public void playerDead(PlayerDeathEvent event){
-        Player player = event.getEntity();
-        String playerName = player.getName();
-
-        player.sendMessage(ChatColor.RED + playerName);
+    public void onDamage(EntityDamageEvent e) {
+        if(e.getEntity() instanceof Player) {
+            Player player = (Player) e.getEntity();
+            if(player.getHealth() <= e.getFinalDamage()){
+                this.PastLocationList = new ArrayList<>();
+                e.setCancelled(true);
+                player.setHealth(20.0);
+                player.teleport(new Location(player.getWorld(), 100, 63, 109));
+                player.sendMessage(ChatColor.RED + "Player " + player.getName() + "is dead");
+            }
+        }
     }
 }
