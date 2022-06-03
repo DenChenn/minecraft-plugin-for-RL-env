740128969cf3afeba7296a50adb5d8df20701cb2
SwarzChen <aaaa102234@gmail.com>
2022-06-03 23:05:20 +0800

    🔥 [main/player] Finish player turning

--- rl-learning-server/test.py
+++ rl-learning-server/test.py
@@ -1,27 +1,31 @@
 import pyautogui
 import time
+import os
 
-swap = 0
+def turn_left():
+    cmd=""" osascript -e '
+    repeat 500 times
+        delay 0.001
+        tell application "System Events" to key code 86 --right
+    end repeat'
+    """
+    os.system(cmd)
+
+
+def turn_right():
+    cmd=""" osascript -e '
+    repeat 500 times
+        delay 0.001
+        tell application "System Events" to key code 88 --right
+    end repeat'
+    """
+    os.system(cmd)
+
+time.sleep(5)
 
 while True:
     time.sleep(2)
-    if swap == 0:
-        print("swap == 0")
-        swap = (swap + 1) % 2
-        currentX, currentY = pyautogui.position()
-        print("X: " + str(currentX) + ", Y: " + str(currentY))
-        # pyautogui.keyDown('e')
-        # pyautogui.keyUp('e')
-        # pyautogui.keyDown('e')
-        # pyautogui.keyUp('e')
-        pyautogui.moveRel(300, 0)
-    else:
-        print("swap == 1")
-        swap = (swap + 1) % 2
-        currentX, currentY = pyautogui.position()
-        print("X: " + str(currentX) + ", Y: " + str(currentY))
-        # pyautogui.keyDown('e')
-        # pyautogui.keyUp('e')
-        # pyautogui.keyDown('e')
-        # pyautogui.keyUp('e')
-        pyautogui.moveRel(- 300, 0)
+    turn_left()
+    time.sleep(2)
+    turn_right()
+
