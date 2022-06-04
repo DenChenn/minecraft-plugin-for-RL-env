8041ce1f321ec3af9d62d0b85caa3bcf1117d915
SwarzChen <aaaa102234@gmail.com>
2022-06-04 16:56:53 +0800

    🔥 [main/action] finish adjusting player action

---
+++ rl-learning-server/action.py
@@ -0,0 +1,47 @@
+import pyautogui
+import time
+import os
+
+def turn_left():
+    cmd=""" osascript -e '
+    repeat 603 times
+        delay 0.001
+        tell application "System Events" to key code 86 --right
+    end repeat'
+    """
+    os.system(cmd)
+
+
+def turn_right():
+    cmd=""" osascript -e '
+    repeat 603 times
+        delay 0.001
+        tell application "System Events" to key code 88 --right
+    end repeat'
+    """
+    os.system(cmd)
+
+def walk_forward_one_block():
+    pyautogui.keyDown('w')
+    time.sleep(0.145)
+    pyautogui.keyUp('w')
+
+def long_jump():
+    pyautogui.press('w')
+    pyautogui.keyDown('w')
+    time.sleep(0.09)
+    pyautogui.keyDown('space')
+    time.sleep(0.01)
+    pyautogui.keyUp('space')
+    time.sleep(0.3)
+    pyautogui.keyUp('w')
+
+def short_jump():
+    pyautogui.keyDown('w')
+    time.sleep(0.03)
+    pyautogui.keyDown('space')
+    time.sleep(0.01)
+    pyautogui.keyUp('space')
+    time.sleep(0.1)
+    pyautogui.keyUp('w')
+
